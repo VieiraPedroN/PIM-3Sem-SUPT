a96b517fb9beaef560c8db1662f602c18ec776d9
Pedro <pedronicolas576@gmail.com>
2025-02-28 11:45:27 -0300

    Adicionar arquivos de projeto.

---
+++ PIM-SUPT/FormLogin.Designer.cs
@@ -0,0 +1,105 @@
+﻿namespace PIM_SUPT
+{
+    partial class FormLogin
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textEmail = new System.Windows.Forms.TextBox();
+            this.textSenha = new System.Windows.Forms.TextBox();
+            this.BtnAcessar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(298, 169);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(45, 16);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "E-mail";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(298, 222);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(46, 16);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Senha";
+            // 
+            // textEmail
+            // 
+            this.textEmail.Location = new System.Drawing.Point(348, 166);
+            this.textEmail.Name = "textEmail";
+            this.textEmail.Size = new System.Drawing.Size(141, 22);
+            this.textEmail.TabIndex = 2;
+            // 
+            // textSenha
+            // 
+            this.textSenha.Location = new System.Drawing.Point(348, 219);
+            this.textSenha.Name = "textSenha";
+            this.textSenha.Size = new System.Drawing.Size(141, 22);
+            this.textSenha.TabIndex = 3;
+            // 
+            // BtnAcessar
+            // 
+            this.BtnAcessar.Location = new System.Drawing.Point(348, 303);
+            this.BtnAcessar.Name = "BtnAcessar";
+            this.BtnAcessar.Size = new System.Drawing.Size(141, 23);
+            this.BtnAcessar.TabIndex = 4;
+            this.BtnAcessar.Text = "Acessar";
+            this.BtnAcessar.UseVisualStyleBackColor = true;
+            this.BtnAcessar.Click += new System.EventHandler(this.BtnAcessar_Click);
+            // 
+            // FormLogin
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.BtnAcessar);
+            this.Controls.Add(this.textSenha);
+            this.Controls.Add(this.textEmail);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "FormLogin";
+            this.Text = "FormLogin";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textEmail;
+        private System.Windows.Forms.TextBox textSenha;
+        private System.Windows.Forms.Button BtnAcessar;
+    }
+}
---
+++ PIM-SUPT/FormLogin.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PIM_SUPT
+{
+    public partial class FormLogin : Form
+    {
+        public FormLogin()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnAcessar_Click(object sender, EventArgs e)
+        {
+            int ID = 0;
+            string emailAdm = "Func";
+            string senhaAdm = "12345";
+
+            string emailFunc = "User";
+            string senhaFunc = "12345";
+
+            if (textEmail.Text.Equals(emailAdm) && textSenha.Text.Equals(senhaAdm))
+            {
+                ID = 1; // Identificador de Funcionario
+            }
+            else if (textEmail.Text.Equals(emailFunc) && textSenha.Text.Equals(senhaFunc))
+            {
+                ID = 2; // Identificador de Usuario
+            }
+
+            if (ID == 1)
+            {
+                FormFuncionario formFunc = new FormFuncionario();
+                formFunc.Show();
+                this.Hide();
+            }
+            else if (ID == 2)
+            {
+                FormUsuario formUsuario = new FormUsuario();
+                formUsuario.Show();
+                this.Hide();
+            }
+            else
+            {
+                MessageBox.Show("Login inválido", "Erro", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+    }
+}
