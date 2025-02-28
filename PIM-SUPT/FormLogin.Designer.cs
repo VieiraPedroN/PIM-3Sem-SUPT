1e1d1392bd272752bf00b05180c2b62a0d4484fe
Pedro <pedronicolas576@gmail.com>
2025-02-28 11:47:53 -0300

    Criando as funcionalidades de login e adicionando ID para validar entre funcionario e usuario

--- PIM-SUPT/FormLogin.Designer.cs
+++ PIM-SUPT/FormLogin.Designer.cs
@@ -81,6 +81,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.MediumPurple;
             this.ClientSize = new System.Drawing.Size(800, 450);
             this.Controls.Add(this.BtnAcessar);
             this.Controls.Add(this.textSenha);
